678c4b816fa83bdb5edbedc9f45ff93d585415a1
Tasos <tasosnikic@gmail.com>
2023-12-19 19:53:28 +0200

    Register Users and Login Logout functionality

--- ecom/ecom/store/views.py
+++ ecom/ecom/store/views.py
@@ -1,12 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 
 # Create your views here.
 def home(request):
     products = Product.objects.all()
-    return render(request, "home.html", {"products":products})
+    return render(request, "home.html", {"products": products})
 
 
 def about(request):
     return render(request, "about.html", {})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error"))
+            return redirect('home')
+    else:
+        return render(request, "login.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out"))
+    return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # login user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have Registered Successfuly"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Error!"))
+            return redirect('register.html')
+    else:
+        return render(request, "register.html", {'form': form})
