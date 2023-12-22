467da6cc45c6bfc84f278cdb24cc2dad1450030c
Tasos <tasosnikic@gmail.com>
2023-12-22 19:25:25 +0200

    First Part Cart app commit

--- ecom/ecom/store/views.py
+++ ecom/ecom/store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -9,6 +9,23 @@ from .forms import SignUpForm
 
 
 # Create your views here.
+def category(request, cat):
+    # replace hyphens with spaces
+    cat = cat.replace("-", " ")
+    # grab category from url
+    try:
+        category = Category.objects.get(name=cat)
+        products = Product.objects.filter(category=category)
+        return render(request, "category.html", {"category": category, "products": products})
+    except:
+        messages.success(request, ("Category does not exist"))
+        return redirect('home')
+
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, "product.html", {"product": product})
+
 def home(request):
     products = Product.objects.all()
     return render(request, "home.html", {"products": products})
