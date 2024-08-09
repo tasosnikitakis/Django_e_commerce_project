410506e04a8f48cca14df5f6a9a6c10e68300bde
Tasos <tasosnikic@gmail.com>
2024-08-09 11:59:52 +0300

    Deployment changes

--- ecom/ecom/ecom/settings.py
+++ ecom/ecom/ecom/settings.py
@@ -69,19 +69,19 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': BASE_DIR / 'db.sqlite3',
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'TCpNBPbTdiiKgGVICQnjdduSgInaeiee',
-#         'HOST': 'postgres.railway.internal',
-#         'PORT': '32868',
-#
-#     }
-# }
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'TCpNBPbTdiiKgGVICQnjdduSgInaeiee',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '32868',
+
+    }
+}
 
 DATABASES = {
 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=1000)
