5f54f69fbb5a6664b264ed5f59a31b670034f313
Tasos <tasosnikic@gmail.com>
2024-09-03 10:01:11 +0300

    railway deployment pt II

--- ecom/ecom/ecom/settings.py
+++ ecom/ecom/ecom/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'rSszdqjyzSAQQYlHdadgqtnBdaodzxjN',
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': 'viaduct.proxy.rlwy.net',
         'PORT': '52229',
 
