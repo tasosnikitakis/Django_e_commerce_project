aec2b6164128ec1ef301a460e594896eb76a8085
Tasos <tasosnikic@gmail.com>
2024-09-07 08:47:48 +0300

    railway deployment pt 10

--- ecom/ecom/ecom/settings.py
+++ ecom/ecom/ecom/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v@8#zyrtska6+s14^_(4+cn7=c1=4k*j8=2k)r!s3&28v^5^g_'
+SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = 'django-insecure-v@8#zyrtska6+s14^_(4+cn7=c1=4k*j8=2k)r!s3&28v^5^g_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
